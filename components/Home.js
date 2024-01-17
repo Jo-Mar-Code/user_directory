72ad24226fe054a7b885af11392563c4ef14d69b
JoM <jordan.marescaux@gmail.com>
2024-01-17 17:24:31 +0100

    API integration & user display

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,36 @@
-import styles from '../styles/Home.module.css';
 import { useEffect, useState } from 'react';
 
-function Home() {
+import Head from 'next/head';
+import Usercard from './Usercard';
 
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?page=1&results=10&nat=fr&seed=abc')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results);
+        setUsers(data.results);
+      });
+  }, []);
+
+  
+  const userCards = users.map((data, i) => {
+    return <Usercard key={i} {...data}/>;
+  });
+  
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Head>
+        <title>User Directory</title>
+      </Head>
+
+      <main className={styles.mainContainer}>
+        {userCards}
       </main>
     </div>
   );
