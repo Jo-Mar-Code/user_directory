b686c49671730e9a83aaa51b2cf365c247c0815b
JoM <jordan.marescaux@gmail.com>
2024-01-18 17:07:17 +0100

    add input users per page

--- components/Home.js
+++ components/Home.js
@@ -1,77 +1,75 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Head from 'next/head';
-import Usercard from './Usercard';
+import Head from "next/head";
+import Usercard from "./Usercard";
 
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 function Home() {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [searchOption, setSearchOption] = useState('2');
+  const [searchOption, setSearchOption] = useState("2");
+  const [nbUsers, setNbUsers] = useState(10);
 
   useEffect(() => {
     if (search) {
-      setUsers(searchResults)
+      setUsers(searchResults);
     } else {
-    fetch(`https://randomuser.me/api/?page=${page}&results=10&nat=fr&seed=fr`)
-      .then(response => response.json())
-      .catch(err => alert(err))
-      .then(data => {
+      fetch(
+        `https://randomuser.me/api/?page=${page}&results=${nbUsers}&nat=fr&seed=fr`
+      )
+        .then((response) => response.json())
+        .catch((err) => alert(err))
+        .then((data) => {
           setUsers(data.results);
-      });
-  }
-}, [page, search]);
+        });
+    }
+  }, [page, search, nbUsers]);
 
   const goToPreviousPage = () => {
-    if (page > 1) { 
+    if (page > 1) {
       setPage(page - 1);
-      setSearch('');
+      setSearch("");
     }
-  }
+  };
 
   const goToNextPage = () => {
-      setPage(page + 1);
-      setSearch('');
-  }
-
+    setPage(page + 1);
+    setSearch("");
+  };
 
   const handleSearch = (searchValue) => {
     console.log(searchOption, searchValue);
-    if (searchOption === '1') {
-    const results = users.filter((result) =>
-    result.name.first.toLowerCase().includes(searchValue.toLowerCase())
-    );
-    setSearchResults(results);
-    } else
-    if (searchOption === '2') {
+    if (searchOption === "1") {
       const results = users.filter((result) =>
-      result.name.last.toLowerCase().includes(searchValue.toLowerCase())
+        result.name.first.toLowerCase().includes(searchValue.toLowerCase())
       );
       setSearchResults(results);
-      } else
-      if (searchOption === '3') {
-        const results = users.filter((result) =>
+    } else if (searchOption === "2") {
+      const results = users.filter((result) =>
+        result.name.last.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setSearchResults(results);
+    } else if (searchOption === "3") {
+      const results = users.filter((result) =>
         result.email.toLowerCase().includes(searchValue.toLowerCase())
-        );
-        setSearchResults(results);
-        }
-    
+      );
+      setSearchResults(results);
+    }
+
     setSearch(searchValue);
-    
-  }
-  
+  };
+
   const userCards = users.map((data, i) => {
-    return <Usercard key={i} {...data}/>;
+    return <Usercard key={i} {...data} />;
   });
 
-  const handleOptionChange = changeEvent => {
-    setSearchOption(changeEvent)
+  const handleOptionChange = (changeEvent) => {
+    setSearchOption(changeEvent);
   };
-  
-  
+
   return (
     <div className={styles.fullPage}>
       <Head>
@@ -79,67 +77,70 @@ function Home() {
       </Head>
 
       <header className={styles.header}>
-       <p className={styles.title}>
-    USER DIRECTORY
-    </p>
-    
-    <div className={styles.search}>
-    <input onChange={(e) => handleSearch(e.target.value)} value={search} placeholder='Search user'/>
-    </div>
+        <p className={styles.title}>USER DIRECTORY</p>
 
-    <div>
-    <div>
-    <label>
-      <input
-        type="radio"
-        name="react-tips"
-        value="1"
-        onChange={(e) => handleOptionChange(e.target.value)}
-        
-      />
-      By firstname
-    </label>
-  </div>
+        <div className={styles.search}>
+          <input
+            onChange={(e) => handleSearch(e.target.value)}
+            value={search}
+            placeholder="Search user"
+          />
+        </div>
 
-  <div>
-    <label>
-      <input
-        type="radio"
-        name="react-tips"
-        value="2"
-        onChange={(e) => handleOptionChange(e.target.value)}
-        
-      />
-      By lastname
-    </label>
-  </div>
+        <div className={styles.radio}>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="selectedOption"
+                value="1"
+                onChange={(e) => handleOptionChange(e.target.value)}
+              />
+              By firstname
+            </label>
+          </div>
 
-  <div>
-    <label>
-      <input
-        type="radio"
-        name="react-tips"
-        value="3"
-        onChange={(e) => handleOptionChange(e.target.value)}
-        
-      />
-      By email
-    </label>
-  </div>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="selectedOption"
+                value="2"
+                onChange={(e) => handleOptionChange(e.target.value)}
+              />
+              By lastname
+            </label>
+          </div>
 
-    </div>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="selectedOption"
+                value="3"
+                onChange={(e) => handleOptionChange(e.target.value)}
+              />
+              By email
+            </label>
+          </div>
+        </div>
 
-  </header>
+        <div className={styles.nbUsers}>
+          <p>Users per page : &nbsp;</p>
+          <input onChange={(e) => setNbUsers(e.target.value)} value={nbUsers} />
+        </div>
+      </header>
 
-      <main className={styles.mainContainer}>
-        {userCards}
-      </main>
+      <main className={styles.mainContainer}>{userCards}</main>
       <div className={styles.nav}>
-        <button className={styles.button} onClick={() => goToPreviousPage()}>Prev. page</button>
+        <button className={styles.button} onClick={() => goToPreviousPage()}>
+          Prev. page
+        </button>
         <p>Page {page}</p>
-        <button className={styles.button} onClick={() => goToNextPage()}>Next page</button>
+        <button className={styles.button} onClick={() => goToNextPage()}>
+          Next page
+        </button>
       </div>
-     
     </div>
   );
 }
--- components/Usercard.js
+++ components/Usercard.js
@@ -1,16 +1,23 @@
-import styles from '../styles/Usercard.module.css';
+import styles from "../styles/Usercard.module.css";
 
 function Usercard(props) {
-
   return (
-  <div className={styles.card} style={{backgroundImage: `linear-gradient(180deg, transparent 0%, rgba(0,0,0,0.5)70%, rgba(0,0,0,0.8) 80%, rgba(0,0,0,1) 100%), url(${props.picture.large})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center'}}>
-    <div className={styles.identity}>
-      <p>{props.name.first}</p>
-      <p>{props.name.last}</p>
-      <p className={styles.email}>{props.email}</p>
+    <div
+      className={styles.card}
+      style={{
+        backgroundImage: `linear-gradient(180deg, transparent 0%, rgba(0,0,0,0.5)70%, rgba(0,0,0,0.8) 80%, rgba(0,0,0,1) 100%), url(${props.picture.large})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className={styles.identity}>
+        <p>{props.name.first}</p>
+        <p>{props.name.last}</p>
+        <p className={styles.email}>{props.email}</p>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Usercard;
