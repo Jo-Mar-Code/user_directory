bb35c1b2dccb6d501130d6b42e8794b2aa65f044
JoM <jordan.marescaux@gmail.com>
2024-01-18 12:06:44 +0100

    add search functionnality & fetch error alert

--- components/Home.js
+++ components/Home.js
@@ -8,26 +8,48 @@ import styles from '../styles/Home.module.css';
 function Home() {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
-
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    fetch(`https://randomuser.me/api/?page=${page}&results=10&nat=fr&seed=abc`)
+    if (search) {
+      setUsers(searchResults)
+    } else {
+    fetch(`https://randomuser.me/api/?page=${page}&results=10&nat=fr&seed=fr`)
       .then(response => response.json())
+      .catch(err => alert(err))
       .then(data => {
-        console.log(data.results);
-        setUsers(data.results);
+          setUsers(data.results);
       });
-  }, [page]);
+  }
+}, [page, search]);
 
-  const firstPage = () => {
+  const goToPreviousPage = () => {
     if (page > 1) { 
       setPage(page - 1);
+      setSearch('');
     }
+  }
+
+  const goToNextPage = () => {
+      setPage(page + 1);
+      setSearch('');
+  }
+
+  const handleSearch = (searchValue) => {
+    const results = users.filter((result) =>
+      result.name.last.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setSearchResults(results);
+    setSearch(searchValue);
+    
   }
   
   const userCards = users.map((data, i) => {
     return <Usercard key={i} {...data}/>;
   });
+
+
   
   return (
     <div className={styles.fullPage}>
@@ -35,13 +57,22 @@ function Home() {
         <title>User Directory</title>
       </Head>
 
+      <header className={styles.header}>
+       <p className={styles.title}>
+    USER DIRECTORY
+    </p>
+    <div className={styles.search}>
+    <input onChange={(e) => handleSearch(e.target.value)} value={search} placeholder='Search user'/>
+    </div>
+  </header>
+
       <main className={styles.mainContainer}>
         {userCards}
       </main>
       <div className={styles.nav}>
-        <button className={styles.button} onClick={() => firstPage()}>Prev. page</button>
+        <button className={styles.button} onClick={() => goToPreviousPage()}>Prev. page</button>
         <p>Page {page}</p>
-        <button className={styles.button} onClick={() => setPage(page + 1)}>Next page</button>
+        <button className={styles.button} onClick={() => goToNextPage()}>Next page</button>
       </div>
      
     </div>
